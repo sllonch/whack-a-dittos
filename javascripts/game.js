7f8b6d69e4f2715b88d07dafa72a2ef51e678e85
kovitx <sllonk@gmail.com>
2018-11-12 17:03:31 +0100

    initial es6 version

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,12 +1,12 @@
-function Game() {
+function Game () {
   this.timeLeft = null;
-  this.lives = 3; 
-  this.score = 0;  
+  this.lives = 3;
+  this.score = 0;
   this.numberHoles = 5;
   this.level = 1;
 }
 
-Game.prototype.start = function() {
+Game.prototype.start = function () {
   this.gameScreen = buildDOM(`
     <main>
       <div class="whole-page">
@@ -55,9 +55,9 @@ Game.prototype.start = function() {
 
   document.body.prepend(this.gameScreen);
 
-  this.soundGame = document.getElementById("audioGame");
-  this.soundSmash = document.getElementById("audioSmash");
-  this.soundDiglett = document.getElementById("audioDiglett");
+  this.soundGame = document.getElementById('audioGame');
+  this.soundSmash = document.getElementById('audioSmash');
+  this.soundDiglett = document.getElementById('audioDiglett');
   this.soundGame.play();
 
   this.timeElement = this.gameScreen.querySelector('.time');
@@ -75,105 +75,104 @@ Game.prototype.start = function() {
   this.gridElement = this.gameScreen.querySelectorAll('.grid-image');
 
   this.holePositions = [];
-  
-  while(this.holePositions.length < this.numberHoles) {
-      this.randomnumber = Math.floor(Math.random() * 25);
-      if(this.holePositions.indexOf(this.randomnumber) > -1) continue;
-      this.holePositions[this.holePositions.length] = this.randomnumber;
+
+  while (this.holePositions.length < this.numberHoles) {
+    this.randomnumber = Math.floor(Math.random() * 25);
+    if (this.holePositions.indexOf(this.randomnumber) > -1) continue;
+    this.holePositions[this.holePositions.length] = this.randomnumber;
   }
 
   this.diglettPosition = Math.floor(Math.random() * this.numberHoles);
   this.diglett = this.holePositions[this.diglettPosition];
 
   this.timePositions = [];
-  
-  while(this.timePositions.length < this.numberHoles) {
-      this.randomnumber = Math.floor(Math.random() * 15) + 5;
-      this.timePositions[this.timePositions.length] = this.randomnumber;
+
+  while (this.timePositions.length < this.numberHoles) {
+    this.randomnumber = Math.floor(Math.random() * 16) + 3;
+    this.timePositions[this.timePositions.length] = this.randomnumber;
   }
-  
-  for(var i = 0; i < 25; i++) {
+
+  for (let i = 0; i < 25; i++) {
     this.gridElement[i].classList.add('ground');
   }
 
-  for(var i = 0; i < this.numberHoles; i++) {
+  for (let i = 0; i < this.numberHoles; i++) {
     this.gridElement[this.holePositions[i]].classList.add('hole');
   }
 
   this.levelElement = this.gameScreen.querySelector('.level');
   this.levelNoElement = this.gameScreen.querySelector('.levelNumber');
-  if(this.level === 5) {
+  if (this.level === 5) {
     this.levelElement.innerText = 'Final level';
   } else this.levelNoElement.innerText = this.level;
 
-  this.handleClick = function(event){
+  this.handleClick = function (event) {
     this.checkHit(event);
-  }.bind(this)
+  }.bind(this);
 
   document.addEventListener('click', this.handleClick);
   this.startTimer();
-}
+};
 
-Game.prototype.startTimer = function() {
-  this.intervalId = setInterval(function() {
+Game.prototype.startTimer = function () {
+  this.intervalId = setInterval(function () {
     this.timeLeft--;
     this.timeElement.innerText = this.timeLeft;
-    for(var i = 0; i < this.numberHoles; i++) {
-      if(this.timeLeft === this.timePositions[i]) this.createPokemon(this.holePositions[i]);
+    for (var i = 0; i < this.numberHoles; i++) {
+      if (this.timeLeft === this.timePositions[i]) this.createPokemon(this.holePositions[i]);
     }
     if (this.timeLeft === 0) {
-      if(this.level === 5) this.finishGame();
+      if (this.level === 5) this.finishGame();
       else {
-      clearInterval(this.intervalId);
-      this.numberHoles = this.numberHoles + 5;
-      this.levelUp();
+        clearInterval(this.intervalId);
+        this.numberHoles = this.numberHoles + 5;
+        this.levelUp();
       }
     }
   }.bind(this), 1000);
-}
+};
 
-Game.prototype.levelUp = function() {
+Game.prototype.levelUp = function () {
   document.removeEventListener('click', this.handleClick);
   this.gameScreen.remove();
   this.level++;
   this.start();
-}
+};
 
-
-Game.prototype.createPokemon = function(position) {  
+Game.prototype.createPokemon = function (position) {
   if (this.diglett === position) this.gridElement[position].classList.add('diglett');
   else {
     this.gridElement[position].classList.add('ditto');
   }
-}
+};
 
-Game.prototype.checkHit = function(event) {
+Game.prototype.checkHit = function (event) {
   this.target = event.target;
   if (this.lives === 1 && this.target.classList.contains('diglett')) {
     this.soundDiglett.play();
     clearInterval(this.intervalId);
     this.finishGame();
-  } else if(this.target.classList.contains('diglett')) { 
+  } else if (this.target.classList.contains('diglett')) {
     this.soundDiglett.play();
     this.page.classList.add('shaky');
     this.target.classList.remove('diglett');
     this.lives--;
     this.livesNoElement.innerText = this.lives;
-  } else if(this.target.classList.contains('ditto')) { 
+  } else if (this.target.classList.contains('ditto')) {
     this.soundSmash.currentTime = 0;
     this.soundSmash.play();
     this.target.classList.remove('ditto');
-    this.score += (this.timeLeft % 25)  * 25;
+    this.score += (this.timeLeft % 25) * 25;
     this.scoreNoElement.innerText = this.score;
   }
-}
+};
 
-Game.prototype.setGameOverCallback = function(callback) {
+Game.prototype.setGameOverCallback = function (callback) {
   this.gameOverCallback = callback;
-}
+};
 
-Game.prototype.finishGame = function() {
+Game.prototype.finishGame = function () {
   document.removeEventListener('click', this.handleClick);
   this.gameScreen.remove();
   this.gameOverCallback();
-}
+};
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,32 +1,32 @@
-'use strict'
+'use strict';
 
-function buildDOM(html) {
+function buildDOM (html) {
   var div = document.createElement('div');
   div.innerHTML = html;
   return div.children[0];
 }
 
-function main() {
+function main () {
   var splashScreen;
   var gameOverScreen;
-  //var highscoresScreen;
+  // var highscoresScreen;
   var startButton;
-  //var highscoresButton;
+  // var highscoresButton;
   var soundGameOver;
   var scoreNoElement;
   var restartButton;
   var gobackButton;
-  //var backButton;
+  // var backButton;
   var rank;
-  //var scores = [];
-  //var scoresObj = {
+  // var scores = [];
+  // var scoresObj = {
   //  name: null,
   //  rank: null,
   //  score: null,
-  //};
+  // };
   var gameOverContainer;
-  
-  function buildSplash() {
+
+  function buildSplash () {
     splashScreen = buildDOM(`
       <main>
         <img class="logo" alt="Whack-a-ditto logo">
@@ -48,15 +48,15 @@ function main() {
           <input class="button" id="instructionsbtn" type="button" value="Show/Hide instructions"> 
         </div>
       </main>
-    `)
-//<input class="button highscores" type="button" value="Highscores">
+    `);
+    // <input class="button highscores" type="button" value="Highscores">
     document.body.prepend(splashScreen);
 
-    var instructionsButton  = document.getElementById('instructionsbtn');
+    var instructionsButton = document.getElementById('instructionsbtn');
     var instructions = document.getElementById('instructions');
     var gif = document.getElementById('game-gif');
 
-    instructionsButton.addEventListener('click', function() {
+    instructionsButton.addEventListener('click', function () {
       instructions.classList.toggle('show');
       gif.classList.toggle('hide');
     });
@@ -64,38 +64,36 @@ function main() {
     startButton = document.querySelector('.start');
     startButton.addEventListener('click', destroySplash);
 
-    //highscoresButton = document.querySelector('.highscores');
-    //highscoresButton.addEventListener('click', destroySplash2);
+    // highscoresButton = document.querySelector('.highscores');
+    // highscoresButton.addEventListener('click', destroySplash2);
   }
 
-  function destroySplash() {
+  function destroySplash () {
     splashScreen.remove();
     startButton.removeEventListener('click', destroySplash);
     buildGameScreen();
   }
 
-  //function destroySplash2() {
+  // function destroySplash2() {
   //  splashScreen.remove();
   //  highscoresButton.removeEventListener('click', destroySplash2);
   //  buildHighscoresScreen();
-  //}
+  // }
 
-  function buildGameScreen() {
+  function buildGameScreen () {
     var game = new Game();
 
     game.start();
     game.setGameOverCallback(function () {
       destroyGameScreen(game.score);
     });
-
   }
 
-  function destroyGameScreen(score) {
+  function destroyGameScreen (score) {
     buildGameOverScreen(score);
   }
 
-  function buildGameOverScreen(score) {
-
+  function buildGameOverScreen (score) {
     gameOverScreen = buildDOM(`
       <main>
         <img class="game-over-logo" alt="Game Over logo">
@@ -111,52 +109,52 @@ function main() {
       </main>  
     `);
 
-    //<div class="center">
+    // <div class="center">
     //  <label for="name" class="text">Name initials (3 characters):</label>
     //  <input class="input" type="text" id="name" name="name" required minlength="3" maxlength="3" size="3">
-    //</div>
+    // </div>
 
     document.body.prepend(gameOverScreen);
 
-    soundGameOver = document.getElementById("audio");
+    soundGameOver = document.getElementById('audio');
     soundGameOver.play();
-    
+
     scoreNoElement = document.querySelector('.scoreNumber');
     scoreNoElement.innerText = score;
-    //scoresObj.score = score;
+    // scoresObj.score = score;
 
     gameOverContainer = document.querySelector('.game-over-container');
     rank = document.querySelector('.rank');
 
-    if(score < 7000) {
+    if (score < 7000) {
       rank.innerText = 'MAGIKARP';
-      //scoresObj.rank = 'MAGIKARP';
+      // scoresObj.rank = 'MAGIKARP';
       gameOverContainer.classList.add('magikarp');
-    } else if(score < 9000) {
+    } else if (score < 9000) {
       rank.innerText = 'PSYDUCK';
-      //scoresObj.rank = 'PSYDUCK';
+      // scoresObj.rank = 'PSYDUCK';
       gameOverContainer.classList.add('psyduck');
-    } else if(score < 11000) {
+    } else if (score < 11000) {
       rank.innerText = 'KADABRA';
-      //scoresObj.rank = 'KADABRA';
+      // scoresObj.rank = 'KADABRA';
       gameOverContainer.classList.add('kadabra');
-    } else if(score < 13000) {
+    } else if (score < 13000) {
       rank.innerText = 'GENGAR';
-      //scoresObj.rank = 'GENGAR';
+      // scoresObj.rank = 'GENGAR';
       gameOverContainer.classList.add('gengar');
-    } else if(score < 15000) {
+    } else if (score < 15000) {
       rank.innerText = 'MEWTWO';
-      //scoresObj.rank = 'MEWTWO';
+      // scoresObj.rank = 'MEWTWO';
       gameOverContainer.classList.add('mewtwo');
     } else {
       rank.innerText = 'MEW';
-      //scoresObj.rank = 'MEW';
+      // scoresObj.rank = 'MEW';
       gameOverContainer.classList.add('mew');
     }
 
-    //scoresObj.name = document.querySelector('.input').value;
-    //scores.push(scoresObj);
-    //localStorage.setItem('score', JSON.stringify(scores[scores.length -1]));
+    // scoresObj.name = document.querySelector('.input').value;
+    // scores.push(scoresObj);
+    // localStorage.setItem('score', JSON.stringify(scores[scores.length -1]));
 
     restartButton = document.querySelector('.restart');
     restartButton.addEventListener('click', destroyGameOverScreen);
@@ -165,8 +163,7 @@ function main() {
     gobackButton.addEventListener('click', destroyGameOverScreen2);
   }
 
-
-  //function buildHighscoresScreen() {
+  // function buildHighscoresScreen() {
 
   //  highscoresScreen = buildDOM(`
   //  <main>
@@ -188,31 +185,31 @@ function main() {
   //      <input class="button back" type="button" value="Back">
   //    </div>
   //  </main>
-  //`)
+  // `)
 
   //  document.body.prepend(highscoresScreen);
 
   //  scoresParsed = JSON.parse(localStorage.getItem('scores'));
-  
+
   //  backButton = document.querySelector('.back');
   //  backButton.addEventListener('click', destroyHighscoresScreen);
-  //}
+  // }
 
-  //function destroyHighscoresScreen() {
+  // function destroyHighscoresScreen() {
   //  highscoresScreen.remove();
   //  backButton.removeEventListener('click', destroyHighscoresScreen);
   //  buildSplash();
-  //}
+  // }
 
-  function destroyGameOverScreen() {
+  function destroyGameOverScreen () {
     gameOverScreen.remove();
-    restartButton.removeEventListener('click', destroyGameOverScreen)
+    restartButton.removeEventListener('click', destroyGameOverScreen);
     buildGameScreen();
   }
 
-  function destroyGameOverScreen2() {
+  function destroyGameOverScreen2 () {
     gameOverScreen.remove();
-    restartButton.removeEventListener('click', destroyGameOverScreen2)
+    restartButton.removeEventListener('click', destroyGameOverScreen2);
     buildSplash();
   }
 
